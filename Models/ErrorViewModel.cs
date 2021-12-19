b272f46eb406d4a429746391cb14306bf034c67c
aycan-acn <aycan.acn2020@gmail.com>
2021-12-19 16:10:33 +0100

    mvc project aangemaakt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Wdpr_Groep_E.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
