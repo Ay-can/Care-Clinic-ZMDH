b356cfa299581a0d1a0eb9bcc9557c64f342c90e
aycan-acn <aycan.acn2020@gmail.com>
2021-12-20 19:27:33 +0100

    demo test, om te kijken of het werkt
    De testen runnen als je in de terminal dotnet test gaat typen.. anders niet.

--- Tests/Webapp.Tests/UnitTest1.cs
+++ Tests/Webapp.Tests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using Wdpr_Groep_E.Controllers;
 using Xunit;
 
 namespace Webapp.Tests
 {
+    //Testen runnen via de terminal, anders doen ze het niet.... In de terminal dotnet test typen.
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-        
+        //Om te kijken of de reference werkt....
+        //HomeController home = new HomeController();
+        Assert.Equal(1,1);
+        }
+
+        [Fact]
+        public void test2()
+        {
+            Assert.Equal("wdpr","wdpr");
         }
     }
 }
