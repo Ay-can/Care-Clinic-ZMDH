93a84ac59d6d7cf2340f3216e4ac9b025cbc7b74
aycan-acn <61593654+aycan-acn@users.noreply.github.com>
2022-01-05 16:39:22 +0100

    Identity gescaffhold

---
+++ Src/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Wdpr_Groep_E.Areas.Identity.IdentityHostingStartup))]
+namespace Wdpr_Groep_E.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Src/Data/AppContext.cs
+++ Src/Data/AppContext.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class AppContext : DbContext
+public class AppContext : IdentityDbContext
 {
 public AppContext(DbContextOptions options) : base(options) {}
 }
