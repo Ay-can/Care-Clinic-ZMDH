5fbfdaa45a053e855934d9faff2fa1d78768dad9
aycan-acn <61593654+aycan-acn@users.noreply.github.com>
2022-01-05 16:40:33 +0100

    migrations voor registreren en inloggen

--- Src/Migrations/20220105153957_IdentityErbijgezet.Designer.cs
+++ Src/Migrations/20220105153957_IdentityErbijgezet.Designer.cs
@@ -2,14 +2,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Wdpr_Groep_E.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20220105153957_IdentityErbijgezet")]
+    partial class IdentityErbijgezet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
