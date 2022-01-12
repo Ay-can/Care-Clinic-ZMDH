c36b5f5a00ca698b18ec201991c3b0f3c6ea4391
Iwan van der Wal <iwan2510@gmail.com>
2022-01-12 21:43:07 +0100

    homepage nieuwe styling en kleine changes

--- Src/wwwroot/js/site.js
+++ Src/wwwroot/js/site.js
@@ -33,3 +33,15 @@ document.onreadystatechange = function () {
         }, 100);
     }
 };
+
+// Smooth scrolling button
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        let target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({ scrollTop: target.offset().top - 64 }, 500);
+        }
+    }
+});
