0485d468d44bd75d64acf960bd54264f1e196a23
Iwan van der Wal <iwan2510@gmail.com>
2022-01-17 00:19:54 +0100

    code opgeschoond

--- Src/Hubs/ChatHub.cs
+++ Src/Hubs/ChatHub.cs
@@ -5,33 +5,6 @@ namespace Wdpr_Groep_E.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
-
-        public Task SendPrivateMessage(string user, string message)
-        {
-            return Clients.User(user).SendAsync("ReceiveMessage", message);
-        }
-
-        public Task SendMessageToGroup(string groupName, string message)
-        {
-            return Clients.Group(groupName).SendAsync("ReceiveMessage", message);
-        }
-
-        public async Task AddToGroup(string groupName)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-
-            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} heeft de groep {groupName} gejoined.");
-        }
-
-        public async Task RemoveFromGroup(string groupName)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-
-            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} heeft de groep {groupName} verlaten.");
-        }
+        public string GetConnectionId() => Context.ConnectionId;
     }
 }
