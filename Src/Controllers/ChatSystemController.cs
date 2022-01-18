1eca3be1dd9f95b883cca867a1cfcf0401d2d078
Iwan van der Wal <iwan2510@gmail.com>
2022-01-18 16:28:33 +0100

    private room code en mail

--- Src/Controllers/ChatSystemController.cs
+++ Src/Controllers/ChatSystemController.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using FluentEmail.Core;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,12 +16,14 @@ namespace Wdpr_Groep_E.Controllers
 {
     public class ChatSystemController : Controller
     {
+        private readonly IFluentEmail _email;
         private readonly UserManager<AppUser> _userManager;
         private readonly ILogger<ChatSystemController> _logger;
         private readonly WdprContext _context;
 
-        public ChatSystemController(ILogger<ChatSystemController> logger, UserManager<AppUser> userManager, WdprContext context)
+        public ChatSystemController(ILogger<ChatSystemController> logger, IFluentEmail email, UserManager<AppUser> userManager, WdprContext context)
         {
+            _email = email;
             _userManager = userManager;
             _context = context;
             _logger = logger;
@@ -29,15 +32,12 @@ namespace Wdpr_Groep_E.Controllers
         public async Task<IActionResult> Index(string search)
         {
             if (User.IsInRole("Orthopedagoog"))
-                return View(await Search(_context.Chats, search).ToListAsync());
+                return View(await Search(_context.Chats.Where(c => c.Type == ChatType.Room), search).ToListAsync());
             else
             {
-                return View(
-                    await Search(
-                        _context.Chats.Include(c => c.Users)
-                        .Where(c => !c.Users.Any(
-                            u => u.UserId == User.FindFirst(ClaimTypes.NameIdentifier).Value)
-                            ), search).ToListAsync());
+                return View(await Search(_context.Chats.Include(c => c.Users)
+                    .Where(c => !c.Users.Any(u => u.UserId == User.FindFirst(ClaimTypes.NameIdentifier).Value) && c.Type == ChatType.Room), search)
+                    .ToListAsync());
             }
         }
 
@@ -84,10 +84,14 @@ namespace Wdpr_Groep_E.Controllers
             var clientId = _userManager.Users.FirstOrDefault(u => u.UserName == name)?.Id;
             if (clientId != null)
             {
-                chat.Users.Add(new ChatUser { UserId = clientId });
+                var sender = _email
+                    .To(_userManager.Users.FirstOrDefault(u => u.UserName == name)?.Email)
+                    .Subject("Chat aanvraag")
+                    .Body($"{_userManager.GetUserAsync(User).Result.UserName} heeft een chat aangevraagd. Gebruik de volgende code om de chat te joinen: {chat.Id}");
                 chat.Users.Add(new ChatUser { UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value });
                 _context.Chats.Add(chat);
                 await _context.SaveChangesAsync();
+                await sender.SendAsync();
                 return RedirectToAction("Chat", "Chat", new { Id = chat.Id });
             }
             else
@@ -96,17 +100,48 @@ namespace Wdpr_Groep_E.Controllers
                     Type = "Failed",
                     Message = "Deze gebruiker bestaat niet.",
                     Redirect = "ChatSystem",
-                    Timeout = 3000
+                    Timeout = 2000
+                });
+        }
+
+        [Authorize(Roles = "Tiener, Kind")]
+        public async Task<IActionResult> JoinPrivateRoom(int id)
+        {
+            var chat = _context.Chats.Include(c => c.Users).FirstOrDefault(c => c.Id == id);
+            if (chat != null)
+            {
+                if (chat.Type == ChatType.Private)
+                {
+                    chat.Users.Add(new ChatUser { UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value });
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Chat", "Chat", new { id = chat.Id });
+                }
+                else
+                    return RedirectToAction("Index", "Message", new
+                    {
+                        Type = "Failed",
+                        Message = "Deze chat is niet privé.",
+                        Redirect = "Chat",
+                        Timeout = 2000
+                    });
+            }
+            else
+                return RedirectToAction("Index", "Message", new
+                {
+                    Type = "Failed",
+                    Message = "Deze chat bestaat niet.",
+                    Redirect = "Chat",
+                    Timeout = 2000
                 });
         }
 
         private int GenerateChatId()
         {
-            var id = new Random().Next(10000, 99999);
+            var id = new Random().Next(100000, 999999);
             while (true)
             {
                 if (_context.Chats.Any(c => c.Id == id))
-                    id = new Random().Next(10000, 99999);
+                    id = new Random().Next(100000, 999999);
                 else
                     break;
             }
